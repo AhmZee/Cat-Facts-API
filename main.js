67b630f6494263b647f50f6e18ca00353342da3c
Ahmed Usman <ahmedusman507@hotmail.com>
2023-10-15 20:05:20 +0100

    fetch background images from api

--- main.js
+++ main.js
@@ -1,18 +1,61 @@
 window.onload = function () {
-  const catAnimationElements = document.querySelectorAll('.cat-animation');
-  let currentImage = 0;
+  // JavaScript code
 
-  setInterval(imgTransform, 7000);
+const catAnimationElements = document.querySelectorAll('.cat-animation');
+let currentImage = 0;
+let catImages = []; // Array to store all cat images
 
-  function imgTransform() {
-      catAnimationElements[currentImage].classList.remove('visible');
-      catAnimationElements[currentImage].classList.add('hidden');
+// Function to fetch cat images from the Unsplash API for a given page
+async function fetchCatImages(page) {
+    const apiUrl = `https://api.unsplash.com/search/photos?page=${page}&query=cat&client_id=5SMnL1Kj3KRgqWUXINstM87ccsK2T2CuSmYP0496-yQ&per_page=12`;
+    return fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => data.results.map(result => result.urls.regular))
+        .catch(error => {
+            console.error('Error fetching cat images:', error);
+        });
+}
 
-      currentImage = (currentImage + 1) % catAnimationElements.length;
+// Function to fetch cat images from all pages
+async function fetchAllCatImages() {
+    const totalPages = 30; // You can adjust the number of pages as needed
+    for (let page = 1; page <= totalPages; page++) {
+        const images = await fetchCatImages(page);
+        catImages = catImages.concat(images);
+    }
+    preloadImages(catImages); // Preload all images
+    changeBackgroundImage(); // Start background animation
+}
 
-      catAnimationElements[currentImage].classList.remove('hidden');
-      catAnimationElements[currentImage].classList.add('visible');
-  }
+// Function to preload all images
+function preloadImages(images) {
+    images.forEach(imageUrl => {
+        const img = new Image();
+        img.src = imageUrl;
+    });
+}
+
+// Function to change the background image smoothly
+function changeBackgroundImage() {
+  const catBackground = document.getElementById('cat-background');
+  const nextImageIndex = Math.floor(Math.random() * catImages.length);
+
+  catBackground.style.opacity = 0; // Fade out the current image
+
+  // Wait for the transition to complete before changing the image
+  setTimeout(() => {
+      catBackground.src = catImages[nextImageIndex];
+      catBackground.style.opacity = 1; // Fade in the next image
+      currentImage = nextImageIndex;
+
+      setTimeout(changeBackgroundImage, 7000); // Change image every 7 seconds
+  }, 2000); // Adjust this delay to match your CSS transition duration
+}
+// Call the function to fetch and display cat images from all pages
+fetchAllCatImages();
+
+
+
 }
 
 const loadElements = document.getElementById('more-fact-btn')
